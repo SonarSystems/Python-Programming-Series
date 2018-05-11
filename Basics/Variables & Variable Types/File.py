afd8f847b3b62a00f2fb3b7752370fcd7c19468d
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-05-11 21:14:51 +0100

    Code

---
+++ Basics/Variables & Variable Types/File.py
@@ -0,0 +1,16 @@
+# cook your dish here
+
+var1 = 500
+var2 = 45.68
+var3 = "Hello World"
+
+a = b = c = 7
+
+print(a)
+print(b)
+print(c)
+
+print(var3)
+print(var3[0])
+print(var3[0:4])
+print(var3[1:])
