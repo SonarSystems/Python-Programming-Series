85f388c613bf79428d685ffe810c7175f32f01c9
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-06-06 15:39:53 +0100

    Code

---
+++ CGI Programming/Basic CGI Programming/File.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+print ("Content-type:text/html\r\n\r\n")
+
+print ("<html>")
+print ("<head>")
+print ("<title>My First CGI-Program </title>")
+print ("<head>")
+print ("<body>")
+print ("<h3>This is HTML's Body section </h3>")
+print ("</body>")
+print ("</html>")
