999e59a509327487da26c9fcdaefbbfa72cb655d
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-07-07 20:12:37 +0100

    Update File.py

--- GUI/Canvas/File.py
+++ GUI/Canvas/File.py
@@ -3,13 +3,13 @@
 import tkinter
 from tkinter import messagebox
 
-top = tkinter.Tk()
+window = tkinter.Tk()
 
-canvasWidget = tkinter.Canvas(top, bg = "red", height = 512, width = 512)
+canvasWidget = tkinter.Canvas(window, bg = "red", height = 512, width = 512)
 
 coord = 10, 50, 512, 512
 arcObject = canvasWidget.create_arc(coord, start = 0, extent = 270, fill = "white")
 lineObject = canvasWidget.create_line(75, 10, 20, 150, fill = 'black')
 canvasWidget.pack()
 
-top.mainloop()
+window.mainloop()
