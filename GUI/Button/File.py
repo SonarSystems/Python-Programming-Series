d30965b382e9cccd667e65e1abcc2f574fe59532
Sonar Systems <SonarSystems@users.noreply.github.com>
2018-07-07 20:12:13 +0100

    Update File.py

--- GUI/Button/File.py
+++ GUI/Button/File.py
@@ -3,12 +3,12 @@
 import tkinter
 from tkinter import messagebox
 
-top = tkinter.Tk()
+window = tkinter.Tk()
 
 def PopupFunction():
    msg = messagebox.showinfo( "Popup Title", "Message")
 
-buttonWidget = tkinter.Button(top, text = "Click Me", command = PopupFunction)
+buttonWidget = tkinter.Button(window, text = "Click Me", command = PopupFunction)
 buttonWidget.place(x = 25,y = 100)
 
-top.mainloop()
+window.mainloop()
